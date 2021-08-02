96c21dee917f0ec51deb7e1341da02a99e160f95
Jim Ke <jimke2000@gmail.com>
2021-08-03 09:55:53 +1000

    Continue importing local stuff

---
+++ libs/updater/lib/latest-version.js
@@ -0,0 +1,20 @@
+const got = require('got') 
+const url = 'https://raw.githubusercontent.com/';
+module.exports = (options, cb) => {
+  if (typeof options.branch === 'undefined') options.branch = 'master';
+  if (typeof options.name === "undefined") {
+      cb(new Error('Please set a name.'));
+      return;
+  }
+  got(url + encodeURIComponent(options.user) + '/' + encodeURIComponent(options.name) + '/' + encodeURIComponent(options.branch) + '/package.json',  (err, data) =>  {
+    if (err === 404) {
+      cb(new Error('Package or version doesn\'t exist on github.'));
+      return;
+    }
+    if (err) {
+      cb(err);
+      return;
+    }
+    cb(null, JSON.parse(data).version);
+  });
+};
