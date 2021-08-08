1a7e2ce86269290278d0529e8ea8a3845962e627
Jim Ke <jimke2000@gmail.com>
2021-08-08 16:34:11 +1000

    Updater

--- libs/updater/index.js
+++ libs/updater/index.js
@@ -2,39 +2,13 @@
 
 const latest = require("./lib/latest-version.js");
 const chalk = require("chalk");
-const semver = require("semver");
-const defaultMessage = function (options) {
-  if (options.latestVersion == options.currentVersion) {
-    return null;
-  } else if (
-    semver.major(options.latestVersion) > semver.major(options.currentVersion)
-  ) {
-    return chalk`{red !} Major update avaliable ({yellow ${options.currentVersion}} ➔ {green ${options.latestVersion}})`;
-  } else if (
-    semver.minor(options.latestVersion) >
-      semver.minor(options.currentVersion) &&
-    semver.major(options.latestVersion) < semver.major(options.currentVersion)
-  ) {
-    return chalk`{yellow !} Feature update avaliable (${options.currentVersion} ➔ {green ${options.latestVersion}})`;
-  } else if (
-    semver.patch(options.latestVersion) >
-      semver.patch(options.currentVersion) &&
-    semver.minor(options.latestVersion) <
-      semver.minor(options.currentVersion) &&
-    semver.major(options.latestVersion) < semver.major(options.currentVersion)
-  ) {
-    return chalk`{cyan !} Patch update avaliable (${options.currentVersion} ➔ ${options.latestVersion})`;
-  } else {
-    return chalk`{cyan β} Experimental version detected! Please be careful...`;
+const latestVersion = require("./lib/latest-version.js");
+
+const defaultMessage = (options) => {
+  if(currentVersion === latestVersion) return null;
+  else {
+    return chalk`{cyan !} An update is avaliable! {gray {yellow ${currentVersion}} => {green ${latestVersion}}`
   }
-  // } else {
-  //   return chalk`{} Update avaliable!`
-  //   // (
-  //   //   "Update available: " +
-  //   //   colors.green(options.latestVersion) +
-  //   //   colors.dim(" (current: " + options.currentVersion + ")")
-  //   // );
-  // }
 };
 module.exports = function (options, cb) {
   latest(options, function (err, latestVersion) {
