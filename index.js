6dc25833496127be0568fac4b039a2be9ce2b964
Jim Ke <jimke2000@gmail.com>
2021-08-09 11:36:57 +1000

    Improved user interface
    User can pick amount of questions to generate

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const updater = require("./libs/updater");
 const problemGen = require("./database/ps");
 const pkg = require("./package.json");
 const pMsg = chalk`{green √} {bold Press any key to continue}`;
-const questionsPerSheet = 50;
+let questionsPerSheet = 50;
 
 /** Check for update */
 updater(
@@ -43,7 +43,7 @@ updater(
           { title: "Subtraction", value: "-" },
           { title: "Multiplication", value: "*" },
           { title: "Division", value: "/" },
-          {title: "Problem Solving", value: "p"}
+          { title: "Problem Solving", description: "", value: "p" },
         ],
       },
       {
@@ -51,8 +51,16 @@ updater(
         name: "range",
         message: chalk`This program generates questions with random numbers from a range. Select the range. {gray (seperate with comma)}`,
       },
+      {
+        type: (prev) => (prev == "p" ? null : "number"),
+        name: "questionsPerSheet",
+        message: chalk`Amount of questions to generate {gray Max. 65, Min. 1}`,
+        validate: (ans) =>
+          ans < 1 || ans > 65 ? chalk`Maximum is 65, minimum is 1` : true,
+      },
     ];
     const res = await prompts(questions);
+    questionsPerSheet = res.questionsPerSheet;
     if (res.range) {
       app(res.type, parseInt(res.range[0]) || 1, parseInt(res.range[1]) || 0);
     } else app(res.type, 1, 10);
@@ -175,7 +183,7 @@ function app(type, h, l) {
     let x = 0;
     let fromTop = 4;
     for (let i = 0; i < questions.length; i++) {
-      if (x % 5 == 0) {
+      if (x % 6 == 0) {
         fromTop += 2;
         x = 0;
       }
