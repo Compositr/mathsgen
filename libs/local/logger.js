e423c26fea869399f9898c30612efdc8c62ffe5e
Jim Ke <jimke2000@gmail.com>
2021-08-13 12:57:56 +1000

    Error msg changes

--- libs/local/logger.js
+++ libs/local/logger.js
@@ -2,5 +2,5 @@ const chalk = require("chalk")
 module.exports.error = (err) => {
   // Unified error handling
   console.log(chalk`{red ×} {bold An error occured. See the message below for more information}`);
-  console.log(chalk`${err}`)
+  console.log(chalk`{red ×} ${err}`);
 }
