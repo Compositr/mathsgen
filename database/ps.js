aafcc40bd9a663d03f2e2a93c34d352482ac23ae
Jim Ke <jimke2000@gmail.com>
2021-08-13 12:58:11 +1000

    Cleanup code

--- database/ps.js
+++ database/ps.js
@@ -6,30 +6,6 @@
  * This file handles math problem generation and handles other math-y stuff.
  */
 
-/**
- * @class
- * Mathamatical Expression object class
- */
-class Expression {
-  /**
-   * Generate a new Expression
-   * @param {String} expr An expression
-   * @param {Number} answer Answer to the expression
-   * @param {Number} first First number
-   * @param {Number} last Last number
-   * @param {String} type Type of question
-   */
-  constructor(expr, answer, first, last, type) {
-    this.expr = expr;
-    this.answer = answer;
-    this.first = first;
-    this.last = last;
-    this.type = type;
-  }
-  evalutate() {
-    return eval(expr);
-  }
-}
 
 
 function rnd(min, max) {
